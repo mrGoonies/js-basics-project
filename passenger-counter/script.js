a66777e96ae81b86f19b1ee2a75a63fe60762037
mrGoonies <gooniesdev@gmail.com>
2025-04-05 10:30:52 -0300

    Adding img and create counter

--- passenger-counter/script.js
+++ passenger-counter/script.js
@@ -1,2 +1,26 @@
-document.getElementById("count-el").innerText = 5;
+let countEl = document.getElementById("count-el")
+console.log(countEl)
+
+let peopleCounter = 0
+
+/**
+ * Increment people counter with 1 plus. 
+ * @returns null
+ */
+function increment() {
+    console.log("The button was clicked")
+    
+    peopleCounter++;
+
+    countEl.innerText = peopleCounter
+
+    console.log(peopleCounter)
+
+    return 
+}  
+
+document.getElementById("count-el").innerText = peopleCounter
+
+
+
 
