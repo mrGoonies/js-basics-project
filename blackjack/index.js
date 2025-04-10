eaaceb0d65320561343abe6e68ebdc1060ceaf46
mrGoonies <gooniesdev@gmail.com>
2025-04-09 22:49:39 -0400

    Mostrando cartas en HTML

--- blackjack/index.js
+++ blackjack/index.js
@@ -6,9 +6,10 @@ let isAlive = true
 let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
-
+let cardsEl = document.querySelector("#cards-el")
 
 function startGame() {
+  cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
   sumEl.textContent = "Sum: " + sum
   if (sum <= 20) {
     message = "Do you want to draw a new card? 🙂"
